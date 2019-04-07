190c3fed0d182c6dfd7481f79a9b3b58c29dffbb
Lee <hgtlzycapps@gmail.com>
2019-04-06 21:09:01 -0400

    190406 study

--- javaScriptHackerRank.js
+++ javaScriptHackerRank.js
@@ -87,3 +87,50 @@ function Day2() {
     solve(meal_cost, tip_percent, tax_percent);
   }
 } //Day2
+
+function Day3(){
+    'use strict';
+
+  process.stdin.resume();
+  process.stdin.setEncoding('utf-8');
+
+  let inputString = '';
+  let currentLine = 0;
+
+  process.stdin.on('data', inputStdin => {
+      inputString += inputStdin;
+  });
+
+  process.stdin.on('end', _ => {
+      inputString = inputString.replace(/\s*$/, '')
+          .split('\n')
+          .map(str => str.replace(/\s*$/, ''));
+
+      main();
+  });
+
+  function readLine() {
+      return inputString[currentLine++];
+  }
+
+  function main() {
+
+      const N = parseInt(readLine(), 10);
+      if (N % 2 == 0) {
+          switch (true) {
+              case (2 <= N && N <= 5):
+                  console.log('Not Weird');
+                  break;
+              case (6 <= N && N <= 20):
+                  console.log('Weird');
+                  break;
+              case (N > 20):
+                  console.log('Not Weird');
+                  break;
+          }
+      } else {
+          console.log('Weird');
+      }
+
+  }//main()
+}
