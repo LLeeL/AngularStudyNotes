701b3b19ccddebee31036a4dc3c733cd8d869ea6
LLeeL <hgtlzycapps@gmail.com>
2019-04-09 09:21:37 -0400

    Day 6 HR study

--- javaScriptHackerRank.js
+++ javaScriptHackerRank.js
@@ -235,3 +235,29 @@ function Day5(){
       }
   }
 }//Day 5
+
+function Day6(){
+  function processData(input) {
+      //Enter your code here
+      let inputCopy = input.split('\n').slice(1);//from the second element
+      inputCopy.forEach((str) => {
+          let evenStr = '';
+          let oddStr = '';
+          for (let i = 0; i < str.length; i++) {
+              i % 2 ? oddStr += str[i] : evenStr += str[i];// 1 is equivalent to true
+          }
+          console.log(`${evenStr} ${oddStr}`);// string conta
+      });
+  } 
+
+  process.stdin.resume();
+  process.stdin.setEncoding("ascii");
+  _input = "";
+  process.stdin.on("data", function (input) {
+      _input += input;
+  });
+
+  process.stdin.on("end", function () {
+    processData(_input);
+  });
+}//Day6
