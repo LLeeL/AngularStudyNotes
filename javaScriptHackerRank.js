5c54eeb5f706aec9ead88f9d65fd119d724ca32a
LLeeL <hgtlzycapps@gmail.com>
2019-04-13 11:31:12 -0400

    Day 10 code and notes

--- javaScriptHackerRank.js
+++ javaScriptHackerRank.js
@@ -382,3 +382,38 @@ function Day9 (){
     }
 
 } //Day 9
+
+//Binary
+function Day10() {
+    'use strict';
+
+    process.stdin.resume();
+    process.stdin.setEncoding('utf-8');
+
+    let inputString = '';
+    let currentLine = 0;
+
+    process.stdin.on('data', inputStdin => {
+        inputString += inputStdin;
+    });
+
+    process.stdin.on('end', _ => {
+        inputString = inputString.replace(/\s*$/, '')
+            .split('\n')
+            .map(str => str.replace(/\s*$/, ''));
+
+        main();
+    });
+
+    function readLine() {
+        return inputString[currentLine++];
+    }
+
+
+
+    function main() {
+        const n = parseInt(readLine(), 10);
+        console.log(Math.max(...Number(n).toString(2).split(/0+/).map(e => e.length)))
+    }
+
+}//Day 10
