610c17f00181bd029eb2f549fde061392e1134f4
Lee <hgtlzycapps@gmail.com>
2019-04-15 12:09:38 -0400

    Day 12 HR

--- javaScriptHackerRank.js
+++ javaScriptHackerRank.js
@@ -474,4 +474,82 @@ function Day11() {
             arr[i] = readLine().split(' ').map(arrTemp => parseInt(arrTemp, 10));
         }
     }
-}
+}//Day 11
+
+
+//about classes and constructors
+function Day12() {
+    'use strict';
+
+    var _input = '';
+    var _index = 0;
+    process.stdin.on('data', (data) => { _input += data; });
+    process.stdin.on('end', () => {
+        _input = _input.split(new RegExp('[ \n]+'));
+        main();    
+    });
+    function read() { return _input[_index++]; }
+    
+    /**** Ignore above this line. ****/
+    
+    class Person {
+        constructor(firstName, lastName, identification) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.idNumber = identification;
+        }
+        
+        printPerson() {
+            console.log(
+                "Name: " + this.lastName + ", " + this.firstName 
+                + "\nID: " + this.idNumber
+            )
+        }
+    }
+    
+    class Student extends Person {
+       
+        constructor(firstName, lastName, id, scores) {
+            super(firstName, lastName, id)
+            this.scores = scores
+        }
+        calculate() {
+            let avg_score = this.scores.reduce(getSum) / this.scores.length
+            if (avg_score >= 90) {
+                return 'O'
+            } else if (avg_score >= 80) {
+                return 'E'
+            } else if (avg_score >= 70) {
+                return 'A'
+            } else if (avg_score >= 55) {
+                return 'P'
+            } else if (avg_score >= 40) {
+                return 'D'
+            } else {
+                return 'T'
+            }
+        }
+    }
+    
+    function getSum(total, num) {
+        return total + num
+    }
+    
+    function main() {
+        let firstName = read()
+        let lastName = read()
+        let id = +read()
+        let numScores = +read()
+        let testScores = new Array(numScores)
+        
+        for (var i = 0; i < numScores; i++) {
+            testScores[i] = +read()  
+        }
+    
+        let s = new Student(firstName, lastName, id, testScores)
+        s.printPerson()
+        s.calculate()
+        console.log('Grade: ' + s.calculate())
+    }
+    
+}//Day12
