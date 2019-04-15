87fa95b5779b938655658e3b3e7cf726e9a73d83
Lee <hgtlzycapps@gmail.com>
2019-04-14 20:35:38 -0400

    Day 11 code study

--- javaScriptHackerRank.js
+++ javaScriptHackerRank.js
@@ -417,3 +417,61 @@ function Day10() {
     }
 
 }//Day 10
+
+function Day11() {
+    'use strict';
+
+    process.stdin.resume();
+    process.stdin.setEncoding('utf-8');
+
+    let inputString = '';
+    let currentLine = 0;
+
+    process.stdin.on('data', inputStdin => {
+        inputString += inputStdin;
+    });
+
+    process.stdin.on('end', _ => {
+        inputString = inputString.replace(/\s*$/, '')
+            .split('\n')
+            .map(str => str.replace(/\s*$/, ''));
+
+        main();
+    });
+
+    function readLine() {
+        return inputString[currentLine++];
+    }
+
+
+
+    function main() {
+        let arr = Array(6);
+
+        for (let i = 0; i < 6; i++) {
+            arr[i] = readLine().split(' ').map(arrTemp => parseInt(arrTemp, 10));
+        }
+
+        //setting highestSum initially to -63 because that is lowest possible outcome for hourglass.
+        //-9 -9 -9
+        //   -9
+        //-9 -9 -9
+        // = -63
+        var highestSum = -63;
+        var tempSum = 0;
+        for (let i = 0; i < 4; i++) {
+            for (let j = 0; j < 4; j++) {
+                tempSum = arr[i][j] + arr[i][j + 1] + arr[i][j + 2] + arr[i + 1][j + 1] + arr[i + 2][j] + arr[i + 2][j + 1] + arr[i + 2][j + 2];
+                if (tempSum > highestSum) {
+                    highestSum = tempSum;
+                }
+            }
+        }
+
+        console.log(highestSum);
+        
+        for (let i = 0; i < 6; i++) {
+            arr[i] = readLine().split(' ').map(arrTemp => parseInt(arrTemp, 10));
+        }
+    }
+}
