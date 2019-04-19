cc1c50134fcb0a4d6c46d996fe4498fb2d39c9c3
Lee <hgtlzycapps@gmail.com>
2019-04-19 18:37:02 -0400

    Day16 HR

--- javaScriptHackerRank.js
+++ javaScriptHackerRank.js
@@ -714,3 +714,37 @@ function Day15(){
         mylist.display(head);
     }		
 }//Day 15
+
+function Day16() {
+    process.stdin.resume();
+    process.stdin.setEncoding('ascii');
+
+    var input_stdin = "";
+    var input_stdin_array = "";
+    var input_currentline = 0;
+
+    process.stdin.on('data', function (data) {
+        input_stdin += data;
+    });
+
+    process.stdin.on('end', function () {
+        input_stdin_array = input_stdin.split("\n");
+        main();    
+    });
+
+    function readLine() {
+        return input_stdin_array[input_currentline++];
+    }
+
+
+    function main() {
+        const S = readLine();
+        try {
+            isNaN(Number(S)) ? err : console.log(S)
+        }
+        catch (err) {
+            console.log('Bad String')
+        }
+    }
+    
+}//Day16
